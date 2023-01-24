fa5fbac07652a950b5972ccef55d06073ffd1ac0
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2023-01-24 21:33:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ import os
 
 
 class Config(object):
-    APP_ID = 17945796
-    API_HASH = "4a05481a5da2d66f801acffc4ca5ee4b"
-    BOT_TOKEN = "5700920410:AAEk8j5NZb6wiIVuu3-NS6AL243bl57EpvU"
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5152809878").split())
+    APP_ID = 
+    API_HASH = 
+    BOT_TOKEN = 
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
     IS_PUBLIC_BOT = os.environ.get("IS_PUBLIC_BOT") in ["True", "true"]
-    LOGS_CHANNEL = int(os.environ.get("-1001874063925")) if os.environ.get("LOGS_CHANNEL") else None
+    LOGS_CHANNEL = int(os.environ.get("")) if os.environ.get("LOGS_CHANNEL") else None
     # DON'T CHANGE THESE 2 VARS
     DOWNLOAD_LOCATION = "./NexaBots"
     TG_MAX_SIZE = 2040108421
     # Mega User Account
-    MEGA_EMAIL = "moharol640@dmtubes.com"
-    MEGA_PASSWORD = "!manuja2"
+    MEGA_EMAIL = 
+    MEGA_PASSWORD = 
